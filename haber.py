45a0765c1283159f64be9fd6b5585921a6849efb
saepsh <81823910+saepsh@users.noreply.github.com>
2021-12-19 21:25:04 +0300

    Add files via upload

---
+++ haber.py
@@ -0,0 +1,30 @@
+import feedparser, time, os
+from colorama import Fore, Back
+os.system("clear")
+url = "http://www.cnnturk.com/feed/rss/news"
+haberler = feedparser.parse(url)
+
+i = 0
+for haber in haberler.entries:
+    i += 1
+    print("\n")
+    print("\n")
+    print(Fore.RED + """
+    __  __      __
+   / / / /___ _/ /_  ___  _____
+  / /_/ / __ `/ __ \/ _ \/ ___/
+ / __  / /_/ / /_/ /  __/ /
+/_/ /_/\__,_/_.___/\___/_/
+
+CODED BY SAEP
+""")
+    print("\n")
+    print(i)
+    print("\n")
+    print(Fore.BLUE + "==> "+haber.title+ " <==")
+    print("\n")
+    print(Fore.GREEN + "URL ==> "+haber.link+" <== URL")
+    print("\n")
+    print(Fore.YELLOW + "DETAY ==> "+haber.summary+" <== DETAY")
+    print("\n")
+    time.sleep(2)
